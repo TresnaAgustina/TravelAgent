109bea83532f3b2a102e1c61c34cfbf144071d24
TresnaAgustina <tresnaa001@gmail.com>
2022-08-25 23:07:32 +0800

    finish tour detail view page

--- resources/views/pages/TourDetail.blade.php
+++ resources/views/pages/TourDetail.blade.php
@@ -17,9 +17,9 @@
                         <img src="{{ asset('assets/img/img1.svg') }}" alt="" class="tour_img">
                   </div>
 
-                  <div class="package_desc px-5 md:px-0 mt-12 md:w-4/5 lg:w-2/3 mx-auto">
+                  <div class="package_desc px-5 md:px-0 mt-12 w-full md:w-4/5 lg:w-2/3 mx-auto">
                         {{-- sub title --}}
-                        <div class="section_title text-center text-md md:text-lg font-bold text-text xl:text-xl">Detal Package
+                        <div class="section_title text-center text-md md:text-lg font-bold text-text xl:text-xl">Detail Package
                               <hr class="thin_line border-s opacity-70 w-4/5 m-auto md:w-1/2 border-primary xl:mt-1 xl:w-4/5">
                               <hr class="bold_line m-auto w-36 opacity-70 border-l rounded-b-md border-primary xl:w-1/4">
                         </div>
@@ -67,7 +67,7 @@
 
                               {{-- price table --}}
                               {{-- start table --}}
-                              <div class="sm:rounded-lg border-primary border-s mt-5 mb-10">
+                              <div class="table_wrap rounded-sm overflow-scroll border-primary border-s mt-5">
                                     <table class="w-full text-sm text-left text-text">
                                     <thead class="text-xs text-text uppercase">
                                           <tr class="bg-primary text-secondary">
@@ -90,6 +90,37 @@
                                     </table>
                               </div>
                               {{-- end table --}}
+
+                              {{-- price include --}}
+                              <div class="info_1 mt-5">
+                                    <h5 class="title font-bold text-md lg:text-lg text-text">Price Include :</h5>
+                                    <ul class="activit mt-2 pl-8">
+                                          <li class="list text-sm lg:text-lg list-disc">Activity 1</li>
+                                          <li class="list text-sm lg:text-lg list-disc my-1">Activity 2</li>
+                                          <li class="list text-sm lg:text-lg list-disc">Activity 3</li>
+                                          <li class="list text-sm lg:text-lg list-disc my-1">Activity 4</li>
+                                          <li class="list text-sm lg:text-lg list-disc">Activity 5</li>
+                                    </ul>
+                              </div>
+
+                              {{-- price exlude --}}
+                              <div class="info_1 mt-5 mb-10">
+                                    <h5 class="title font-bold text-md lg:text-lg text-text">Price Exlude :</h5>
+                                    <ul class="activit mt-2 pl-8">
+                                          <li class="list text-sm lg:text-lg list-disc">Activity 1</li>
+                                          <li class="list text-sm lg:text-lg list-disc my-1">Activity 2</li>
+                                          <li class="list text-sm lg:text-lg list-disc">Activity 3</li>
+                                          <li class="list text-sm lg:text-lg list-disc my-1">Activity 4</li>
+                                          <li class="list text-sm lg:text-lg list-disc">Activity 5</li>
+                                    </ul>
+                              </div>
+
+                              {{-- button group --}}
+                              <div class="btn_group">
+                                    <a href="#" class="gform"><button class="btn_gform w-full h-10 rounded-sm bg-primary text-secondary font-medium md:font-bold text-sm md:text-base hover:bg-hover">Book Now (Via Google Form)</button></a>
+                                    <p class="text-center font-bold text-base lg:text-lg text-paragraph my-2">Or</p>
+                                    <a href="#" class="wa"><button class="btn_wa mb-10 w-full h-10 rounded-sm border-s border-primary hover:text-primary font-medium text-sm md:text-base md:font-semibold text-paragraph">Chat Via Whatsapp</button></a>
+                              </div>
                         </div>
                   </div>
             </div>
